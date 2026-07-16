9d70a27cfd1326091621472895ea4a1a1abbe67e
Shatha <s.alghamdiwork@gmail.com>
2026-07-16 17:34:07 +0300

    Adding the calculater.c

---
+++  simple-calculator/calculator.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+
+int addition(int a, int b)
+{
+    return a + b;
+}
+
+int subtraction(int a, int b)
+{
+    return a - b;
+}
+
+int multiplication(int a, int b)
+{
+    return a * b;
+}
+
+int division(int a, int b)
+{
+    if (b != 0)
+    {
+        return a / b;
+    }
+    else
+    {
+        printf("Error: Division by zero is not allowed.\n");
+        return -1; // Return an error value
+    }
+}
+
+int main(void)
+{
+    int a, b, menuselection;
+    printf(" welcome to the simple calculator\n");
+    printf(" Simple Calculator \n 1) Add \n  2) Subtract \n 3) Multiply \n 4) Divide \n 0) Quit");
+    scanf("%d", &menuselection);
+    if (menuselection == 1)
+    {
+         printf("Enter two numbers: ");
+         scanf("%d %d", &a, &b);
+        printf("Sum of %d and %d is %d\n", a, b, addition(a, b));
+    }
+    else if (menuselection == 2)
+    {
+         printf("Enter two numbers: ");
+         scanf("%d %d", &a, &b);
+        printf("Difference of %d and %d is %d\n", a, b, subtraction(a, b));
+    }
+    else if (menuselection == 3)
+    {
+            printf("Enter two numbers: ");
+            scanf("%d %d", &a, &b);
+        printf("Product of %d and %d is %d\n", a, b, multiplication(a, b));
+    }
+    else if (menuselection == 4)
+    {
+        printf("Enter two numbers: ");
+        scanf("%d %d", &a, &b);
+        int result = division(a, b);
+        if (result != -1) // Check for division by zero
+        {
+            printf("Quotient of %d and %d is %d\n", a, b, result);
+        }
+    }
+    else if (menuselection == 0)
+    {
+        printf("Exiting the calculator.\n");
+    }
+    else
+    {
+        printf("Invalid choice. Please try again.\n");
+    }
+    // switch(menuselection)
+    // {
+    //     case 1:
+    //         c = a + b;
+    //         printf("Sum of %d and %d is %d\n", a, b, c);
+    //         break;
+    //     case 2:
+    //         c = a - b;
+    //         printf("Difference of %d and %d is %d\n", a, b, c);
+    //         break;
+    //     case 3:
+    //         c = a * b;
+    //         printf("Product of %d and %d is %d\n", a, b, c);
+    //         break;
+    //     case 4:
+    //         if(b != 0)
+    //         {
+    //             c = a / b;
+    //             printf("Quotient of %d and %d is %d\n", a, b, c);
+    //         }
+    //         else
+    //         {
+    //             printf("Error: Division by zero is not allowed.\n");
+    //         }
+    //         break;
+    //     case 0:
+    //         printf("Exiting the calculator.\n");
+    //         break;
+    //     default:
+    //         printf("Invalid choice. Please try again.\n");
+    // }
+    return 0;
+}

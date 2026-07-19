9154568a04fda7a47562cbc01a45377c387b5103
Shatha <s.alghamdiwork@gmail.com>
2026-07-19 15:33:38 +0000

    Add _strspn function

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to scan
+ * @accept: accepted characters
+ *
+ * Return: number of bytes in initial segment
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, found;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		found = 0;
+
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				found = 1;
+				break;
+			}
+		}
+
+		if (found == 0)
+		{
+			break;
+		}
+	}
+
+	return (i);
+}

eea9c1646d3cee0e3099c8067106ca823725c450
Shatha <s.alghamdiwork@gmail.com>
2026-07-20 15:16:25 +0000

    fixing bettey

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -7,17 +7,16 @@
  *
  * Return: pointer to allocated memory
  */
-
 void *malloc_checked(unsigned int b)
 {
-    void *ptr;
+	void *ptr;
 
-    ptr = malloc(b);
+	ptr = malloc(b);
 
-    if (ptr == NULL)
-    {
-        exit(98);
-    }
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
 
-    return (ptr);
+	return (ptr);
 }

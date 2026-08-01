63234cd342df092613d5075bfc9fca6c513fad4d
Shatha <s.alghamdiwork@gmail.com>
2026-08-01 05:31:56 +0300

    fixing betty

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,28 +1,26 @@
 #include "variadic_functions.h"
 #include <stdio.h>
-
 /**
  * print_numbers - prints numbers
  * @separator: string to print between numbers
  * @n: number of integers
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i;
+	va_list args;
+	unsigned int i;
 
-    va_start(args, n);
+	va_start(args, n);
 
-    for (i = 0; i < n; i++)
-    {
-        printf("%d", va_arg(args, int));
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
 
-        if (separator != NULL && i != n - 1)
-            printf("%s", separator);
-    }
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+	}
 
-    va_end(args);
+	va_end(args);
 
-    printf("\n");
+	printf("\n");
 }

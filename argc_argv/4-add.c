66be755f1be51a55e685a171ac4f45390db2d9cb
Shatha <s.alghamdiwork@gmail.com>
2026-08-02 00:04:37 +0300

    adding extra info to 4- add file

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -26,7 +26,27 @@ int main(int argc, char *argv[])
 		}
 		sum += atoi(argv[i]); /*atoi() stands for ASCII to Integer.
 
-It is a function from the C standard library (stdlib.h) that converts a string into an integer.*/
+It is a function from the C standard library (stdlib.h) that converts a string into an integer.
+the manuel way ...... 
+	sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		number = 0;
+
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+			number = number * 10 + (argv[i][j] - '0');
+		}
+
+		sum += number;
+	}*/
 	}
 
 	printf("%d\n", sum);

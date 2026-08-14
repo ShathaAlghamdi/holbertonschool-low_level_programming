d3a89d52c6af9a46c019790e267d08c9a0a778d3
Shatha <61396210+ShathaAlghamdi@users.noreply.github.com>
2026-08-15 02:16:30 +0300

    fixing betty in task3 and pushing task 4

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,4 +1,33 @@
 #include "hash_tables.h"
+#include <stdlib.h>
+/**
+ * create_hash_node - creates a new hash node
+ * @key: key for the node
+ * @value: value for the node
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
+static hash_node_t *create_hash_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+
+	node->key = strdup(key);
+	node->value = strdup(value);
+	if (node->key == NULL || node->value == NULL)
+	{
+		free(node->key);
+		free(node->value);
+		free(node);
+		return (NULL);
+	}
+
+	node->next = NULL;
+	return (node);
+}
 
 /**
  * hash_table_set - adds an element to the hash table
@@ -11,14 +40,13 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *node;
+	hash_node_t *node, *new;
 	char *new_value;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-
 	node = ht->array[index];
 
 	while (node != NULL)
@@ -28,36 +56,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			new_value = strdup(value);
 			if (new_value == NULL)
 				return (0);
-
 			free(node->value);
 			node->value = new_value;
 			return (1);
 		}
-
 		node = node->next;
 	}
 
-	node = malloc(sizeof(hash_node_t));
-	if (node == NULL)
+	new = create_hash_node(key, value);
+	if (new == NULL)
 		return (0);
 
-	node->key = strdup(key);
-	if (node->key == NULL)
-	{
-		free(node);
-		return (0);
-	}
-
-	node->value = strdup(value);
-	if (node->value == NULL)
-	{
-		free(node->key);
-		free(node);
-		return (0);
-	}
-
-	node->next = ht->array[index];
-	ht->array[index] = node;
+	new->next = ht->array[index];
+	ht->array[index] = new;
 
 	return (1);
 }

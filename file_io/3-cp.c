c5f1d94764f53bb2a0d52b7cec7b2d077fa23576
Shatha <61396210+ShathaAlghamdi@users.noreply.github.com>
2026-08-24 17:44:42 +0300

    fixing the betty style for task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,14 +1,14 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * close_fd - closes a file descriptor
- * @fd: file descriptor
+ * close_file - closes a file descriptor
+ * @fd: file descriptor to close
  *
  * Return: void
  */
-void close_fd(int fd)
+void close_file(int fd)
 {
 	if (close(fd) == -1)
 	{
@@ -20,17 +20,15 @@ void close_fd(int fd)
 
 /**
  * main - copies the content of one file to another
- * @argc: argument count
- * @argv: argument vector
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-	int fd_from;
-	int fd_to;
-	ssize_t bytes_read;
-	ssize_t bytes_written;
+	int fd_from, fd_to;
+	ssize_t rd, wr;
 	char buffer[1024];
 
 	if (argc != 3)
@@ -50,49 +48,43 @@ int main(int argc, char *argv[])
 
 	fd_to = open(argv[2],
 		O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO,
 			"Error: Can't write to %s\n", argv[2]);
-		close_fd(fd_from);
+		close_file(fd_from);
 		exit(99);
 	}
 
-	bytes_read = read(fd_from, buffer, 1024);
+	rd = read(fd_from, buffer, 1024);
 
-	while (bytes_read > 0)
+	while (rd > 0)
 	{
-		bytes_written = write(fd_to, buffer, bytes_read);
+		wr = write(fd_to, buffer, rd);
 
-		if (bytes_written == -1 ||
-		    bytes_written != bytes_read)
+		if (wr == -1 || wr != rd)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n",
-				argv[2]);
-
-			close_fd(fd_from);
-			close_fd(fd_to);
+				"Error: Can't write to %s\n", argv[2]);
+			close_file(fd_from);
+			close_file(fd_to);
 			exit(99);
 		}
 
-		bytes_read = read(fd_from, buffer, 1024);
+		rd = read(fd_from, buffer, 1024);
 	}
 
-	if (bytes_read == -1)
+	if (rd == -1)
 	{
 		dprintf(STDERR_FILENO,
-			"Error: Can't read from file %s\n",
-			argv[1]);
-
-		close_fd(fd_from);
-		close_fd(fd_to);
+			"Error: Can't read from file %s\n", argv[1]);
+		close_file(fd_from);
+		close_file(fd_to);
 		exit(98);
 	}
 
-	close_fd(fd_from);
-	close_fd(fd_to);
+	close_file(fd_from);
+	close_file(fd_to);
 
 	return (0);
 }

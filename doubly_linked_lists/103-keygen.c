66073cddbbaec295e8dca552df255bc871cfdf88
Shatha <61396210+ShathaAlghamdi@users.noreply.github.com>
2026-09-02 21:02:20 +0300

    fixing betty

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -3,6 +3,111 @@
 #include <string.h>
 
 /**
+ * get_table - returns the crackme character table
+ *
+ * Return: pointer to the character table
+ */
+char *get_table(void)
+{
+	static char table[] = {
+		'A', '-', 'C', 'H', 'R', 'D', 'w', '8',
+		'7', 'l', 'N', 'S', '0', 'E', '9', 'B',
+		'2', 'T', 'i', 'b', 'g', 'p', 'n', 'M',
+		'V', 'y', 's', '5', 'X', 'z', 'v', 't',
+		'O', 'G', 'J', 'c', 'Y', 'L', 'U', '+',
+		'4', 'm', 'j', 'W', '6', 'f', 'x', 'q',
+		'Z', 'e', 'F', '3', 'Q', 'a', '1', 'r',
+		'P', 'h', 'd', 'K', 'I', 'o', 'u', 'k'
+	};
+
+	return (table);
+}
+
+/**
+ * key_sum - calculates the sum-based key index
+ * @name: username
+ * @length: username length
+ *
+ * Return: key index
+ */
+unsigned int key_sum(char *name, unsigned int length)
+{
+	unsigned int sum = 0, i;
+
+	for (i = 0; i < length; i++)
+		sum += (unsigned char)name[i];
+	return ((sum ^ 79u) & 63u);
+}
+
+/**
+ * key_product - calculates the product-based key index
+ * @name: username
+ * @length: username length
+ *
+ * Return: key index
+ */
+unsigned int key_product(char *name, unsigned int length)
+{
+	unsigned int product = 1, i;
+
+	for (i = 0; i < length; i++)
+		product *= (unsigned char)name[i];
+	return ((product ^ 85u) & 63u);
+}
+
+/**
+ * key_max - calculates the maximum-character key index
+ * @name: username
+ * @length: username length
+ *
+ * Return: key index
+ */
+unsigned int key_max(char *name, unsigned int length)
+{
+	unsigned int maximum = (unsigned char)name[0], i;
+
+	for (i = 0; i < length; i++)
+		if ((unsigned char)name[i] > maximum)
+			maximum = (unsigned char)name[i];
+	srand(maximum ^ 14u);
+	return ((unsigned int)rand() & 63u);
+}
+
+/**
+ * key_squares - calculates the square-sum key index
+ * @name: username
+ * @length: username length
+ *
+ * Return: key index
+ */
+unsigned int key_squares(char *name, unsigned int length)
+{
+	unsigned int squares = 0, value, i;
+
+	for (i = 0; i < length; i++)
+	{
+		value = (unsigned char)name[i];
+		squares += value * value;
+	}
+	return ((squares ^ 239u) & 63u);
+}
+
+/**
+ * key_random - calculates the final pseudo-random key index
+ * @name: username
+ *
+ * Return: key index
+ */
+unsigned int key_random(char *name)
+{
+	unsigned int value = 0, i;
+
+	for (i = 0; i < (unsigned char)name[0]; i++)
+		value = (unsigned int)rand();
+	return ((value ^ 229u) & 63u);
+}
+
+/**
  * main - generates a valid key for crackme5
  * @argc: number of command-line arguments
  * @argv: array of command-line arguments
@@ -11,46 +116,19 @@
  */
 int main(int argc, char **argv)
 {
-	char table[] = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+"
-		"4mjW6fxqZeF3Qa1rPhdKIouk";
-	char *username;
-	unsigned int length, sum, product, maximum, squares, random_value;
-	unsigned int i;
+	char *table = get_table();
+	char *name;
+	unsigned int length;
 
 	if (argc != 2)
 		return (1);
-
-	username = argv[1];
-	length = (unsigned int)strlen(username);
+	name = argv[1];
+	length = (unsigned int)strlen(name);
 	printf("%c", table[(length ^ 59u) & 63u]);
-
-	sum = 0;
-	product = 1;
-	maximum = (unsigned char)username[0];
-	squares = 0;
-	for (i = 0; i < length; i++)
-	{
-		unsigned int value = (unsigned char)username[i];
-
-		sum += value;
-		product *= value;
-		if (value > maximum)
-			maximum = value;
-		squares += value * value;
-	}
-
-	printf("%c", table[(sum ^ 79u) & 63u]);
-	printf("%c", table[(product ^ 85u) & 63u]);
-
-	srand(maximum ^ 14u);
-	printf("%c", table[(unsigned int)rand() & 63u]);
-
-	printf("%c", table[(squares ^ 239u) & 63u]);
-
-	random_value = 0;
-	for (i = 0; i < (unsigned char)username[0]; i++)
-		random_value = (unsigned int)rand();
-	printf("%c", table[(random_value ^ 229u) & 63u]);
-
+	printf("%c", table[key_sum(name, length)]);
+	printf("%c", table[key_product(name, length)]);
+	printf("%c", table[key_max(name, length)]);
+	printf("%c", table[key_squares(name, length)]);
+	printf("%c", table[key_random(name)]);
 	return (0);
 }
